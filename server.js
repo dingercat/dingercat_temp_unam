60e292a63c0a8cf865ec69f1c4bff1567992e23c
Dingercat <bogiepop@gmail.com>
2026-03-27 14:38:28 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const app = express();
 
 app.use(express.json());
 
-// Arreglo donde se guardan las tareas en memoria
 const todos = [];
 
-// Endpoint para AGREGAR tarea
 app.post('/agrega_todo', (req, res) => {
   const { todo } = req.body;
 
@@ -14,14 +12,13 @@ app.post('/agrega_todo', (req, res) => {
     return res.status(400).json({ error: 'El todo no puede estar vacío' });
   }
 
-  // Guarda la tarea en el arreglo
   todos.push(todo);
-  console.log('Tareas guardadas:', todos);
+  console.log('Todo recibido:', todo);
 
   res.status(201).json({ mensaje: 'Todo guardado exitosamente', todo });
 });
 
-// ✅ Endpoint nuevo para VER todas las tareas
+// ✅ Endpoint para ver todas las tareas
 app.get('/todos', (req, res) => {
   res.status(200).json({ tareas: todos });
 });
@@ -29,13 +26,3 @@ app.get('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Servidor corriendo en puerto 3000');
 });
-```
-
----
-
-## ¿Cómo ver las tareas?
-
-### Opción 1: Desde el navegador
-Entra a esta URL directamente:
-```
-https://animated-eureka-4j6jqwpxjp6hqg7r-3000.app.github.dev/todos
